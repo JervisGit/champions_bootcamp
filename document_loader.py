101ef55dfbc80bdd7476993569d02b7c74322b23
jervisGit <jlee045@mymail.sim.edu.sg>
2024-10-12 15:41:16 +0800

    try smaller chunks

--- document_loader.py
+++ document_loader.py
@@ -4,17 +4,13 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def load_documents(directory):
     documents = []
-    #for filename in os.listdir(directory):
-    #    if filename.endswith('.pdf'):
-    #        file_path = os.path.join(directory, filename)
-    #        loader = PyPDFLoader(file_path)
-    #        documents.extend(loader.load())
-    filename = "iit_faq.pdf"
-    file_path = os.path.join(directory, filename)
-    loader = PyPDFLoader(file_path)
-    documents.extend(loader.load())
+    for filename in os.listdir(directory):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(directory, filename)
+            loader = PyPDFLoader(file_path)
+            documents.extend(loader.load())
     
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=350, chunk_overlap=50)
     split_documents = text_splitter.split_documents(documents)
     
     return split_documents
--- rag_pipeline.py
+++ rag_pipeline.py
@@ -8,12 +8,12 @@ load_dotenv()
 
 def create_rag_pipeline(vector_store):
     llm = ChatGroq(temperature=0.5, model_name="mixtral-8x7b-32768")
-    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+    retriever = vector_store.as_retriever(search_kwargs={"k": 5})
     
     rag_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=retriever,
-        #return_source_documents=True
+        return_source_documents=True
     )
     
     return rag_chain
