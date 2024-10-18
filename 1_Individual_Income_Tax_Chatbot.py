340f10edf3224a9948303aa7ad572b8fd32d8a05
jervisGit <jlee045@mymail.sim.edu.sg>
2024-10-18 22:35:12 +0800

    add disclaimer, about us page and methodology page

--- 1_Individual_Income_Tax_Chatbot.py
+++ 1_Individual_Income_Tax_Chatbot.py
@@ -10,6 +10,15 @@ import os
 st.title("🤖 Chatbot for Individual Income Tax Filing")
 st.caption("🚀 An AI Champions Bootcamp Project")
 
+with st.expander("Disclaimer"):
+    st.write('''
+        IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.
+
+        Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.
+
+        Always consult with qualified professionals for accurate and personalized advice.
+    ''')
+
 # Load environment variables from .env file
 load_dotenv()
 
---
+++ pages/3_About_Us.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("About Us")
+st.caption("🚀 An AI Champions Bootcamp Project")
